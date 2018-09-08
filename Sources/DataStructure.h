8d29228496f8b52258a2f6e5d9ac674e2e022848
Bensalah <amine.bensalah@outlook.com>
2018-09-08 12:09:49 +0200

    init project

---
+++ Sources/DataStructure.h
@@ -0,0 +1,18 @@
+//
+//  DataStructure.h
+//  DataStructure
+//
+//  Created by Amine Bensalah on 08/09/2018.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataStructure.
+FOUNDATION_EXPORT double DataStructure_VersionNumber;
+
+//! Project version string for DataStructure.
+FOUNDATION_EXPORT const unsigned char DataStructure_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructure/PublicHeader.h>
+
+
---
+++ Sources/Node.swift
@@ -0,0 +1,222 @@
+import Foundation
+
+final public class Node<T> {
+    
+    var value: T
+    var children: [Node] = []
+    weak var parent: Node?
+    
+    var isRoot: Bool {
+        return parent == nil
+    }
+    
+    var hasChildren: Bool {
+        return !children.isEmpty
+    }
+    
+    public var numberOfChildren: Int {
+        return children.count
+    }
+    
+    public var level: Int {
+        if let parent = parent {
+            return parent.level + 1
+        }
+        return 0
+    }
+    
+    public var isLeaf: Bool {
+        return numberOfChildren < 1
+    }
+    
+    public var indexPath: IndexPath {
+        if let parent = parent {
+            let parentPath = parent.indexPath
+            if let childIndex = parent.index(for: self) {
+                return parentPath.appending(childIndex)
+            }
+        }
+        return IndexPath(index: level)
+    }
+    
+    init(value: T, parent: Node? = nil) {
+        
+        precondition(Thread.isMainThread)
+        
+        self.value = value
+        self.parent = parent
+    }
+    
+    @discardableResult
+    func createNode(value: T) -> Node? {
+        let node = Node(value: value, parent: self)
+        children.append(node)
+        return node
+    }
+    
+    func add(child: Node) {
+        children.append(child)
+        child.parent = self
+    }
+    
+    public func child(at index: Int) -> Node? {
+        if index >= children.count || index < 0 {
+            return nil
+        }
+        return children[index]
+    }
+    
+    public func index(for node: Node) -> Int? {
+        return children.index { (oneChildNode) -> Bool in
+            return oneChildNode === node
+        }
+    }
+    
+    public func hasAncestor(in nodes: [Node]) -> Bool {
+        for node in nodes {
+            if node.isAncestor(of: self) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    public func isAncestor(of node: Node) -> Bool {
+        if node == self {
+            return false
+        }
+        var nomad = node
+        while true {
+            guard let parent = nomad.parent else {
+                return false
+            }
+            if parent == self {
+                return true
+            }
+            nomad = parent
+        }
+    }
+}
+
+extension Node: Equatable {
+    public class func == (lhs: Node, rhs: Node) -> Bool {
+        return lhs === rhs
+    }
+    
+    public func contain(_ node: Node) -> Bool {
+        if self == node {
+            return true
+        }
+        for child in children {
+            if child.contain(node) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    public func find(where: (Node) -> Bool) -> Node? {
+        if `where`(self) {
+            return self
+        }
+        
+        for child in children {
+            if let found = child.find(where: `where`) {
+                return found
+            }
+        }
+        
+        return nil
+    }
+    
+    public func find(_ node: Node) -> Node? {
+        if self == node {
+            return self
+        }
+        
+        for child in children {
+            if let find = child.find(node) {
+                return find
+            }
+        }
+        
+        return nil
+    }
+    
+    @discardableResult
+    public func remove(_ node: Node) -> Node? {
+        if let index = children.index(where: { $0 == node }) {
+            children[index].parent = nil
+            return children.remove(at: index)
+        }
+        
+        for child in children {
+            if let removeNode = child.remove(node) {
+                return removeNode
+            }
+        }
+        
+        return nil
+    }
+    
+    @discardableResult
+    public func insert(_ node: Node, in newNode: Node) -> Bool {
+        if let new = find(newNode) {
+            new.add(child: node)
+            return true
+        }
+        return false
+    }
+    
+    @discardableResult
+    public func move(_ node: Node, to new: Node) -> Bool {
+        if let child = find(node), let parentChild = child.parent {
+            parentChild.remove(child)
+            return self.insert(child, in: new)
+        }
+        return false
+    }
+}
+
+extension Node: CustomStringConvertible {
+    public var description: String {
+        var text = "\(value) at \(indexPath) for level \(level) \n"
+        text += "\((0...level).map { _ in return " " })"
+        
+        if !children.isEmpty {
+            text += " {" + children.map { $0.description }.joined(separator: ", ") + "} "
+        }
+        
+        return text
+    }
+}
+
+extension Node where T: Equatable {
+    public func search(value: T) -> Node? {
+        if value == self.value {
+            return self
+        }
+        
+        for child in children {
+            if let found = child.search(value: value) {
+                return found
+            }
+        }
+        
+        return nil
+    }
+    
+    public func search(where: (T) -> Bool) -> Node? {
+        if `where`(self.value) {
+            return self
+        }
+        
+        for child in children {
+            if let found = child.search(where: `where`) {
+                return found
+            }
+        }
+        
+        return nil
+    }
+}
